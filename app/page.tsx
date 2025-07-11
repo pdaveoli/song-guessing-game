b165d2fb8096ca1fee00ccf7992475808b617f2c
pdaveoli <pdaveoli@monkswalk.herts.sch.uk>
2025-07-11 14:41:10 +0100

    More suspense

--- app/page.tsx
+++ app/page.tsx
@@ -8,11 +8,14 @@ import { SignUpUserSteps } from "@/components/tutorial/sign-up-user-steps";
 import { hasEnvVars } from "@/lib/utils";
 import AuthHandler from "@/components/auth-handler";
 import Link from "next/link";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center">
+      <Suspense fallback={<div className="flex-1 flex items-center justify-center">Loading...</div>}>
       <AuthHandler />
+      </Suspense>
       <div className="flex-1 w-full flex flex-col gap-20 items-center">
         <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
           <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
